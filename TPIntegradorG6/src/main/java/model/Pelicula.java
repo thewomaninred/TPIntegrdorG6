c62a7a2ba6356242f191fd96819922dba33c15ab
thewomaninred <valeriacerroniperea@gmail.com>
2023-12-08 09:45:44 -0300

    Dependencias correctas Esquema de paquetes Esquema de carpetas Modelo cargado

--- TPIntegradorG6/src/main/java/model/Pelicula.java
+++ TPIntegradorG6/src/main/java/model/Pelicula.java
@@ -4,10 +4,123 @@
  */
 package model;
 
+import java.util.Date;
+
 /**
  *
  * @author Vale
  */
 public class Pelicula {
+    private int idPelicula;
+    private String Actores;
+    private int Año;
+    private String Director;
+    private Date duracion;
+    private String Genero;
+    private Byte Imagen;
+    private String Sinopsis;
+    private String Titulo;
+
+    public Pelicula(int idPelicula, String Actores, int Año, String Director, Date duracion, String Genero, Byte Imagen, String Sinopsis, String Titulo) {
+        this.idPelicula = idPelicula;
+        this.Actores = Actores;
+        this.Año = Año;
+        this.Director = Director;
+        this.duracion = duracion;
+        this.Genero = Genero;
+        this.Imagen = Imagen;
+        this.Sinopsis = Sinopsis;
+        this.Titulo = Titulo;
+    }
+
+    public Pelicula(String Actores, int Año, String Director, Date duracion, String Genero, Byte Imagen, String Sinopsis, String Titulo) {
+        this.Actores = Actores;
+        this.Año = Año;
+        this.Director = Director;
+        this.duracion = duracion;
+        this.Genero = Genero;
+        this.Imagen = Imagen;
+        this.Sinopsis = Sinopsis;
+        this.Titulo = Titulo;
+    }
+
+    public Pelicula() {
+    }
+
+    
+    
+    public int getIdPelicula() {
+        return idPelicula;
+    }
+
+    public void setIdPelicula(int idPelicula) {
+        this.idPelicula = idPelicula;
+    }
+
+    public String getActores() {
+        return Actores;
+    }
+
+    public void setActores(String Actores) {
+        this.Actores = Actores;
+    }
+
+    public int getAño() {
+        return Año;
+    }
+
+    public void setAño(int Año) {
+        this.Año = Año;
+    }
+
+    public String getDirector() {
+        return Director;
+    }
+
+    public void setDirector(String Director) {
+        this.Director = Director;
+    }
+
+    public Date getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Date duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+
+    public Byte getImagen() {
+        return Imagen;
+    }
+
+    public void setImagen(Byte Imagen) {
+        this.Imagen = Imagen;
+    }
+
+    public String getSinopsis() {
+        return Sinopsis;
+    }
+
+    public void setSinopsis(String Sinopsis) {
+        this.Sinopsis = Sinopsis;
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String Titulo) {
+        this.Titulo = Titulo;
+    }
+    
+    
     
 }
