ae513dbfaa0ad01de69f234c01fbd0458d5bed2c
sagunti <98052298+sagunti@users.noreply.github.com>
2023-12-13 19:35:04 -0300

    funcion filtrar pelis

--- TPIntegradorG6/src/main/webapp/js/GetRequests.js
+++ TPIntegradorG6/src/main/webapp/js/GetRequests.js
@@ -28,6 +28,40 @@ document.addEventListener("DOMContentLoaded", function(){
                     
                 });
     }
+    
+
+    function filterPelis(palabra){
+        const pelisFiltrados = pelis.filter(pelis=>{
+            return pelis.titulo.toLowerCase().includes(palabra.toLowerCase());
+        });
+        peliCard.innerHTML = "";
+        
+        pelisFiltrados.forEach( peli=>{
+            const card = document.createElement("div");
+            card.className = "col-md-3 mb-4 ident";
+            card.setAttribute("data-peli-id",peli.idPelicula);
+            card.innerHTML = `
+            <div class="card h-100 animate-hover-card">
+                           <div class="card h-100 animate-hover-card bg-light"> 
+                            <img src="data:image/jpeg;base64,${peli.imagenBase64}" class="card-img-top h-75" alt="Imagen portada">
+                                <div class="card-body">
+                                   <h5 class="card-title"> ${peli.titulo}</h5>
+                                   <p class="card-text">${peli.sinopsis}</p>
+                                </div>
+                            </div>
+                            </div>
+                        `
+            peliCard.appendChild(card);
+        });
+    }
+   
+    // Evento que lanza la funcion de filtrar y agregar tarjetas de libros
+   const searchForm = document.querySelector("form[role='search']");
+    searchForm.addEventListener("submit",function(e){        
+        e.preventDefault();
+        const searchTerm = searchForm.querySelector("input[type='search']").value; 
+        filterPelis(searchTerm)
+        });
     loadPelisList();
 });
     
